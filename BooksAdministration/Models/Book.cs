109575d182d6b8802ed890d39c0adb34d0fb9eaf
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-12-10 13:50:08 -0400

    Create book model

---
+++ BooksAdministration/Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksAdministration.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(
+            50, 
+            ErrorMessage = "The {0} should be {2} and max {1}", 
+            MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(
+            100,
+            ErrorMessage = "The {0} should be {2} and max {1}",
+            MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        [Display(Name = "Creation Date")]
+        [DataType(DataType.Date)]
+        public DateTime Created_At { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        [StringLength(
+            50,
+            ErrorMessage = "The {0} should be {2} and max {1}",
+            MinimumLength = 2)]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [StringLength(
+            50,
+            ErrorMessage = "The {0} should be {2} and max {1}",
+            MinimumLength = 2)]
+        public int Price { get; set; }
+    }
+}
