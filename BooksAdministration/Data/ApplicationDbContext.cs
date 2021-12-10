994632e6f49d7d54cf74e805b4b1ab2363806fd6
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-12-10 15:35:13 -0400

    migration

---
+++ BooksAdministration/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BooksAdministration.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAdministration.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }   
+}
--- BooksAdministration/Models/Book.cs
+++ BooksAdministration/Models/Book.cs
@@ -35,10 +35,6 @@ namespace BooksAdministration.Models
         public string Author { get; set; }
 
         [Required(ErrorMessage = "Price is required")]
-        [StringLength(
-            50,
-            ErrorMessage = "The {0} should be {2} and max {1}",
-            MinimumLength = 2)]
         public int Price { get; set; }
     }
 }
