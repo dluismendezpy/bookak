61d386904009198e625e945f0de3a281505386ec
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-12-10 15:35:02 -0400

    Databse configuration

--- BooksAdministration/Startup.cs
+++ BooksAdministration/Startup.cs
@@ -1,6 +1,8 @@
+using BooksAdministration.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +24,11 @@ namespace BooksAdministration
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   
+            // ICustomFormatter Databse configuration
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
