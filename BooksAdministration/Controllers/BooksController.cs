0dda9d11120cdc24ef1464e650e5d04ebcf47b50
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2021-12-11 09:51:41 -0400

    Create BooksController.cs

---
+++ BooksAdministration/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using BooksAdministration.Data;
+using BooksAdministration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BooksAdministration.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Book> BookList = _context.Book;
+            return View(BookList);
+        }
+        
+        // HTTP GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+    }
+}
